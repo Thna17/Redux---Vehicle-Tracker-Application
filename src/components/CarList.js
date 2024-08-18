ee44619d20b4eda726eb316f75f587c4d26b5077
Hong Than Brathna <anbschool0021@AnBs-21-Mac-mini.local>
2024-08-18 18:44:39 +0700

    Listing the Record

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,7 +1,29 @@
-
+import { useSelector } from "react-redux"
 const CarList = () => {
+    const cars = useSelector((state) => {
+        return state.cars.data;
+    })
+    const handleCarDelete = (car) => {
+        // Assuming this is a function to remove car from the store
+        console.log(`Deleting car: ${car.name} - ${car.cost}`)
+        // dispatch(removeCar(car.id))
+    }
+    const renderedCarList = cars.map((car) => {
+        return (
+            <div key={car.id} className="panel">
+                <p>
+                    {car.name} - {car.cost}
+                </p>
+                <button onClick={() => handleCarDelete(car)} className="button is-danger">
+                    Delete
+                </button>
+            </div>
+        )
+    })
   return (
-    <div>CarList</div>
+    <div className="car-list">
+        {renderedCarList}
+        </div>
   )
 }
 
--- src/store/slices/carsSlice.js
+++ src/store/slices/carsSlice.js
@@ -4,7 +4,7 @@ const carsSlice = createSlice({
     name: 'cars',
     initialState: {
         searchTerm: '',
-        cars: []
+        data: []
     },
     reducers: {
         changeSearchTerm(state, action) {
@@ -13,7 +13,7 @@ const carsSlice = createSlice({
         addCar(state, action) {
             // Assumption:
             //action.payload === {name: 'cars', cost: 40}
-            state.cars.push({
+            state.data.push({
                 name: action.payload.name,
                 cost: action.payload.cost,
                 id: nanoid()
@@ -22,7 +22,7 @@ const carsSlice = createSlice({
         removeCar(state, action) {
             // Assumption:
             // action.payload == the id of the car we want to remove
-            const update = state.cars.filter((car) => {
+            const update = state.data.filter((car) => {
                 return car.id !== action.payload
             })
             state.cars = update;
