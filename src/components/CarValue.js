5e166b253e8cac446061284eb3456a4678a02d0b
Hong Than Brathna <anbschool0021@AnBs-21-Mac-mini.local>
2024-08-18 20:57:46 +0700

    total car cost

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,7 +1,22 @@
+import { useSelector } from "react-redux"
 
 const CarValue = () => {
-  return (
-    <div>CarValue</div>
+    const totalCost = useSelector(({cars: {data, searchTerm}}) => {
+        const filteredCar = data.filter((car) => {
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        })
+        let cost = 0;
+        // for (let car of filteredCar) {
+        //     cost += car.cost;
+        // }
+        // return cost;
+
+        return filteredCar.reduce((acc, car) => acc + car.cost, 0)
+    })
+    return (
+        <div className="car-value">
+            Total Cost: ${totalCost}
+        </div>
   )
 }
 
