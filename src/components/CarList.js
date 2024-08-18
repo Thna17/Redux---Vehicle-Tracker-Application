7a8b6e8205ef9f223dd24278502cf67e47d9cd42
Hong Than Brathna <anbschool0021@AnBs-21-Mac-mini.local>
2024-08-18 20:46:10 +0700

    Derived State in useSelector

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -2,8 +2,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store";
 const CarList = () => {
     const dispatch = useDispatch();
-    const cars = useSelector((state) => {
-        return state.cars.data;
+    const cars = useSelector(({cars: {data, searchTerm}}) => {
+        return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
     })
     const handleCarDelete = (car) => {
         // Assuming this is a function to remove car from the store
